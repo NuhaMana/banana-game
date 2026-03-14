a7601a4ac7f9d2b4b0cebf0a28c06543cc96a653
Nuha <dev@work.com>
2026-03-14 20:41:33 +0530

    added functionality to game.js

---
+++ js/game.js
@@ -0,0 +1,40 @@
+let currentAnswer = 0;
+let score = 0;
+
+async function loadQuestion() {
+    try {
+        let response = await fetch("php/fetch_banana.php");
+        let data = await response.json();
+
+        if (data.error) {
+            document.getElementById("message").innerText = data.error;
+            return;
+        }
+
+        document.getElementById("bananaImage").src = data.question;
+        currentAnswer = data.solution;
+        document.getElementById("answer").value = "";
+        document.getElementById("message").innerText = "";
+    } catch (error) {
+        document.getElementById("message").innerText = "Error loading question";
+    }
+}
+
+function checkAnswer() {
+    let userAnswer = document.getElementById("answer").value;
+
+    if (userAnswer === "") {
+        document.getElementById("message").innerText = "Please enter an answer";
+        return;
+    }
+
+    if (parseInt(userAnswer) === parseInt(currentAnswer)) {
+        score++;
+        document.getElementById("score").innerText = score;
+        document.getElementById("message").innerText = "Correct!";
+    } else {
+        document.getElementById("message").innerText = "Wrong! Correct answer was " + currentAnswer;
+    }
+}
+
+loadQuestion();
