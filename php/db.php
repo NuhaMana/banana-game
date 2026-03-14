08295a53d65df8a81617c6704c3404557f737464
Nuha <dev@work.com>
2026-03-14 22:19:23 +0530

    fixed code in db.php so that pages are loaded well

--- php/db.php
+++ php/db.php
@@ -1,10 +1,11 @@
 <?php
-$host = "localhost";
+$host = "127.0.0.1";
+$port = 4306;
 $user = "root";
 $pass = "";
 $dbname = "banana_game";
 
-$conn = new mysqli($host, $user, $pass, $dbname);
+$conn = new mysqli($host, $user, $pass, $dbname, $port);
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
